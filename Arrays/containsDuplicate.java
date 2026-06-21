fa286f0f8928440ad81239989c1cc4c9e9c2add8
seetha04x <seetha04x@gmail.com>
2026-06-21 19:05:51 +0530

    Add solution

---
+++ Arrays/containsDuplicate.java
@@ -0,0 +1,20 @@
+package Arrays;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+public class conatinsDuplicate {
+    public boolean containsDuplicate(int[] nums){
+    Arrays.sort(nums);
+        for(int i=1;i<nums.length;i++){
+            if(nums[i]==nums[i-1]) return true;
+        }
+        return false;
+    } 
+    // HashSet<Integer> seen=new HashSet<>();
+    // for(int i=0;i<nums.length;i++){
+    //     if (seen.contains(nums[i])) return true;
+    //         seen.add(nums[i]);
+    //     }
+    //     return false;   
+}
