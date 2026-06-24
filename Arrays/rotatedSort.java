295d274d4c0852d41ec50f35e2bcd56ca88f0342
seetha04x <seetha04x@gmail.com>
2026-06-24 18:29:58 +0530

    Add solution for rotated sort using recursion

--- Arrays/rotatedSort.java
+++ Arrays/rotatedSort.java
@@ -16,5 +16,26 @@ public class rotatedSort {
             }
         }
         return -1;
-    }    
+    } 
+    
+    public static int rotatedSort(int arr[], int target,int left,int right){
+        if(left>right) return -1;
+
+        int mid=(left+right)/2;
+
+        if(arr[mid]==target) return mid;
+
+        if(arr[mid]<=arr[right]){
+            if(target<=arr[right] && target>arr[mid]){
+                return rotatedSort(arr, target, mid+1, right);
+            }else {return rotatedSort(arr, target, left, mid-1);
+            }
+        }
+        else {
+            if(target<arr[mid] && target>=arr[left]){
+                return rotatedSort(arr, target, left, mid-1);
+            }else {return rotatedSort(arr, target, mid+1, right);
+            }
+        } 
+    }  
 }
