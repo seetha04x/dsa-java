f9ca7340e91b1b5871aff05a9d3a01bacff443cb
seetha04x <seetha04x@gmail.com>
2026-08-13 23:10:50 +0530

    add greedy soln

---
+++ chocola.java
@@ -0,0 +1,41 @@
+import java.util.Arrays;
+import java.util.Collections;
+
+public class chocola {
+    public static void main(String[] args) {
+        int n=4,m=6;
+
+        Integer costVer[]={2,1,3,1,4};
+        Integer costHor[]={4,1,2};
+
+        Arrays.sort(costHor,Collections.reverseOrder());
+        Arrays.sort(costVer,Collections.reverseOrder());
+
+        int hp=1,vp=1;
+        int h=0,v=0;
+        int cost=0;;
+
+        while(v<costVer.length && h<costHor.length){
+            if(costVer[v]<=costHor[h]){
+                hp++;
+                cost+=(costHor[h]*vp);
+                h++;
+            }else{
+                vp++;
+                cost+=(costVer[v]*hp);
+                v++;
+            }
+        }
+         while(v<costVer.length){
+            vp*=2;
+            cost+=(costVer[v]*hp);
+            v++;
+         }
+         while (h<costHor.length) {
+            hp*=2;
+            cost+=(costHor[h]*vp);
+            h++;
+         }
+         System.out.println(cost);
+    }
+}
